64bd29688a38b7ba89cbaeebb062420500134f81
AnishaGhosh-14 <anishaaa1444@gmail.com>
2022-08-14 10:47:58 +0530

    update style

--- src/components/BestSellerCard.jsx
+++ src/components/BestSellerCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Link, typography } from '@mui/material';
+import { Box, Link,  typography } from '@mui/material';
 
 
 const bestSellerCard = ({ bestSeller }) => {
@@ -7,26 +7,36 @@ const bestSellerCard = ({ bestSeller }) => {
 
   return (
     <>
-
+     
       {/* {console.log(bestSeller)} */}
-
+     
       <Link to={`/book/${title}`}>
-        <div class="container">
-          <img className='book-img' src={book_image} alt={title} loading='lazy' />
-          <p className='rank'>
-            Rank: {rank}
-          </p>
+      <div class="container">
+  {/* <div class="row">
+    <div class="center col-lg-6"> */}
+       
+        <img className='book-img' src={book_image} alt={title} loading='lazy' />
+        <p className='rank_one'>
+          #{rank}
+       </p>
 
-          <p className='rank_title'>
-            {title}
-          </p>
+      
 
-          <p className='rank_author' >{author}</p>
-          
+        < p className='rank_title'>
+          {title}
+        </ p>
+        
+        {/* <div class="center col-lg-6"> */}
+        < p className='rank_author' >{author}</ p>
+        {/* </div> */}
+        {/* </div>
+        </div> */}
         </div>
       </Link>
 
-    </>
+    
+       
+      </>
   )
 }
 
